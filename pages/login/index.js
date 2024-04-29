3542938550e0d94e242b47e8d311deef11dad0d1
lynakimhay <nakimhay.ly@institute.pse.ngo>
2024-04-29 10:54:14 +0700

    login js

---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn");
+
+const url = 'http://127.0.0.1:5500/pages/reaction/index.html?';
+
+btn.addEventListener("click",() =>{
+    window.location.href = url;
+});
+
+
+
